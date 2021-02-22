6b4a6cae72656ec8efb193ccd51c0a79130c9f55
Helena Rasche <hxr@hx42.org>
2021-02-22 11:11:24 +0100

    add submodules, workflow installation

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -4,6 +4,10 @@
 git fetch origin
 git reset --hard origin/master
 
+# Update submodules
+git submodule sync
+git submodule update --init --recursive
+
 # Dependencies
 if [[ ! -d ~/.cache/tools-venv ]] ; then
 	virtualenv ~/.cache/tools-venv
@@ -23,3 +27,6 @@ git push
 
 # Apply the changes
 make install GALAXY_SERVER=http://bioinf-galactus/ GALAXY_API_KEY=$GALAXY_ADMIN_API_KEY
+
+# Install workflows
+find . -name '*.ga' -exec workflow-install -g http://bioinf-galactus/ -a $GALAXY_ADMIN_API_KEY -w '{}' \;
