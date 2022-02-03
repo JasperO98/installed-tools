e64b4cf80f694531e3c5326eed840967f998ef03
Helena Rasche <hxr@hx42.org>
2022-02-03 10:53:28 +0100

    make it work

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -14,6 +14,9 @@ fi
 . ~/.cache/tools-venv/bin/activate
 pip install -r requirements.txt
 
+# "fix them"
+make fix
+
 # Apply the changes
 make install GALAXY_SERVER=http://bioinf-galactus/ GALAXY_API_KEY=$GALAXY_ADMIN_API_KEY
 
